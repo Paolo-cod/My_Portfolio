72a4a00dc048c706c016bbe34f59edd5394fa745
DELOFON S. Paolo <sehome.delofon@etu.univ-amu.fr>
2025-08-05 23:02:34 +0200

    Met à jour le README et ajoute tous les fichiers (html, css, js, images, videos) nécessaires à la présentation du portfolio

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# My_Portfolio
-Ceci est mon portfolio
+Bienvenue sur mon portfolio personnel !
+Ce site présente mes projets, compétences et expériences en développement web / design / développement applications GUI en C++ / algorithmique
+
+Technologies utilisées
+
+HTML5 / CSS3
+Bootstrap
+JavaScript
+Git & GitHub Pages pour l’hébergement
---
+++ src/index.js
@@ -0,0 +1,15 @@
+/*function showDiv(idDiv){
+    let divIds = ['projetsPersonnels', 'projetsProfessionnels'];
+    divIds.forEach(divId => {
+        let div = document.getElementById(divId);
+        if (divId === idDiv){
+            div.style.display = (div.style.display === "none") ? "none" : "block";
+        }else{
+            div.style.display = "none";
+        }
+    });
+}
+
+document.getElementById("btn-personal").addEventListener("click", function () { showDiv("projetsPersonnels"); });
+document.getElementById("btn-professional").addEventListener("click", function () { showDiv("projetsProfessionnels"); });
+*/
